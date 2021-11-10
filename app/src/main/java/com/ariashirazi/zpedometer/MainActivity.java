955ddc1635ddf14f7c7513dc71e8c5fc81f0dc28
Aria shirazi <ariashirazi7@gmail.com>
2021-11-10 16:17:11 +0330

    Design Pedometer Main Page UI

--- app/src/main/java/com/ariashirazi/zpedometer/MainActivity.java
+++ app/src/main/java/com/ariashirazi/zpedometer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ariashirazi.zpedometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
 
 
