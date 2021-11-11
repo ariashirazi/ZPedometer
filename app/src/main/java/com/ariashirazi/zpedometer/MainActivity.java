d53d84aab4a4d7a5ed96ac58f1d85c108c5a0b0a
Aria shirazi <ariashirazi7@gmail.com>
2021-11-11 10:14:02 +0330

    Complete Pedometer Main Page UI Design

--- app/src/main/java/com/ariashirazi/zpedometer/MainActivity.java
+++ app/src/main/java/com/ariashirazi/zpedometer/MainActivity.java
@@ -2,10 +2,17 @@ package com.ariashirazi.zpedometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isOpenMoreRapidCV = false;
+    private CardView moreCV;
+    private TextView moreTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +21,48 @@ public class MainActivity extends AppCompatActivity {
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        setupView();
+        onClick();
+
 
 
     }
+
+    private void onClick() {
+
+        moreTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openAndCloseMoreRapidLL();
+
+            }
+        });
+
+    }
+
+    private void setupView() {
+
+        moreCV = findViewById(R.id.moreCV);
+        moreTV = findViewById(R.id.moreTV);
+
+    }
+
+    public void openAndCloseMoreRapidLL(){
+
+        if (isOpenMoreRapidCV){
+
+            moreCV.animate().translationY(320).setDuration(500).start();
+            isOpenMoreRapidCV = false;
+
+        }else {
+
+            moreCV.animate().translationY(32).setDuration(500).start();
+            isOpenMoreRapidCV = true;
+
+        }
+
+    }
+
+
 }
